8294a7305e8fb2bfb85d0843ae0338470356441b
Uranus <whlisikuang@outlook.com>
2020-12-06 05:55:52 +0800

    添加项目文件。

---
+++ LinearAlgebra/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"matrix.h"
+using namespace std;
+double a[9]={1,0,0,0,2,0,0,0,3};
+int main()
+{
+	Matrix A=Matrix(3,3,a);
+	cout<<A;
+	return 0;
+}
---
+++ LinearAlgebra/matrix.h
@@ -0,0 +1,44 @@
+#include<iostream>
+#pragma once
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+class Matrix
+{
+private:
+	unsigned int row,column;
+	double *value;
+	unsigned int get_pos(const unsigned int &,const unsigned int &) const;
+public:
+	Matrix();
+	Matrix(unsigned int,unsigned int,double[]);
+	~Matrix();
+	Matrix row_echelon() const;
+	Matrix reduced_row_echelon() const;
+	friend std::ostream& operator<<(std::ostream &,const Matrix &);
+	friend std::istream& operator>>(std::istream &,Matrix &);
+	friend Matrix operator+(const Matrix &,const Matrix &);
+	friend Matrix operator-(const Matrix &,const Matrix &);
+	friend Matrix operator*(const double &,const Matrix &);
+	friend Matrix operator*(const Matrix &,const Matrix &);
+	friend Matrix identity_matrix(const unsigned int &);
+	unsigned int get_row() const;
+	unsigned int get_column() const;
+	unsigned int size() const;
+	void clear();
+	bool empty() const;
+	double get_element(const unsigned int &,const unsigned int &) const;
+	void set_element(const unsigned int &,const unsigned int &,const double &);
+	void set_size(const unsigned int &,const unsigned int &);
+	double determinant() const;
+	double cominor(const unsigned int &,const unsigned int &) const;
+	double trace() const;
+	Matrix power(unsigned int) const;
+	Matrix transpose() const;
+	Matrix adjugate() const;
+	Matrix inverse() const;
+	Matrix solve_linear_equation() const;
+};
+
+#endif
